f3949b54f88beaafc2d7a39c5a07d7c529814dd1
Harika-kancharla <kancharlaharika0406@gmail.com>
2025-04-01 17:05:53 +0530

    first commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const userRoutes = require("./routes/userRoutes");
 dotenv.config();
 app.use(cors());
 app.use(express.json());
-app.use(cors({ origin: "https://reliable-kangaroo-40dcb5.netlify.app", credentials: true }));
+app.use(cors({ origin: "https://digitalyou.netlify.app", credentials: true }));
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB connected successfully"))
